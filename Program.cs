5f35dc383d3e1d107722979b7b4633b9d8488339
Bogdan <hurasy71@gmail.com>
2022-12-19 19:21:39 +1000

    Task 2

--- Program.cs
+++ Program.cs
@@ -1,1 +1,14 @@
-﻿       
+﻿  System.Console.Write("Введите первое число");     
+  int number1 = Convert.ToInt32(System.Console.ReadLine());
+
+  System.Console.Write("Введите второе число");
+  int number2 = Convert.ToInt32(System.Console.ReadLine());
+
+  if (number1 > number2)
+{
+   System.Console.WriteLine("первое число большее, а второе меньшее"); 
+}
+  else
+  {
+    System.Console.WriteLine("первое число меньшее, а второе большее");
+  } 
